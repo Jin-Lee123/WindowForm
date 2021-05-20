db4b33001e49bbcc34020560d9ff74b93f334838
Jin-Lee123 <wls4310@koreatech.ac.kr>
2021-05-20 14:30:10 +0900

    2021-05-20오전커

---
+++ Appdevelop/Appdevelop/FM_Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appdevelop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;    //로그인 화면을 보이지 않게 한다.
+            FM_PassWord fmpassword = new FM_PassWord();
+            fmpassword.ShowDialog();
+            this.Visible = true;     //화면닫으면 다시 로그인 화면 나옴!
+        }
+    }
+}
---
+++ Appdevelop/Appdevelop/FM_PassWord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Appdevelop
+{
+    public partial class FM_PassWord : Form
+    {
+        private SqlConnection Connect = null;         // 데이터 베이스 접속 정보
+        private SqlTransaction Tran;                  // 데이터 베이스 관리 권한
+        private SqlCommand cmd = new SqlCommand();    // 데이터 베이스 명령 전달
+        public FM_PassWord()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 비밀 번호를 변경한다.
+            string strCon = "Data Source=(local);Initial Catalog=AppDev; Integrated Security=true;";
+            Connect = new SqlConnection(strCon);
+
+            Connect.Open(); //데이터 베이스에 접속 한다.
+
+            // 1. ID 존재여부 확인
+            // 2. 이전 비밀번호가 일치하는지 확인
+            // 3. 바뀔 비밀번호로 등록
+            // 4. 변경 여부 메세지 처리
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
